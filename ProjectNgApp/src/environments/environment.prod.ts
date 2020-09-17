f7e162c50e9605014be8b814ef7b97eff87656a9
Kiran Gandhi <Kirangandhi@outlook.com>
2020-09-18 00:24:26 +0530

    prod build changes

--- ProjectNgApp/src/environments/environment.prod.ts
+++ ProjectNgApp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCrUNOceml3_OKxhJfrNwNkUey4-TU7y8Y",
+    authDomain: "project-ng-app-909ad.firebaseapp.com",
+    databaseURL: "https://project-ng-app-909ad.firebaseio.com",
+    projectId: "project-ng-app-909ad",
+    storageBucket: "project-ng-app-909ad.appspot.com",
+    messagingSenderId: "959151839650",
+    appId: "1:959151839650:web:7b7d2b4ff2b52a49013afb",
+    measurementId: "G-TT0W2S5E9C"
+  }
 };
